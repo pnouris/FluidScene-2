e9db96cda7af1cf66686d40a623c3b1a1d21a00f
UFOID <projectce@tutanota.de>
2017-03-15 18:30:33 +0000

    added texture

--- src/main/java/VaseShape.java
+++ src/main/java/VaseShape.java
@@ -1,5 +1,6 @@
 
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PShape;
 import processing.core.PVector;
 
@@ -20,8 +21,10 @@ public class VaseShape {
         x = m.width/2;
         y = 600;
         speed = 5;
-        vase.rotateX(m.radians(180));
-        vase.rotateY(m.radians(90));
+
+        this.vase = createShapeTri(vase);
+        this.vase.rotateX(m.radians(180));
+        this.vase.rotateY(m.radians(90));
         //vase.rotateZ(m.radians(10));
 
     }
@@ -63,6 +66,29 @@ public class VaseShape {
         m.rotateY((float) 4.66);
         m.shape(vase);
         m.popMatrix();
+    }
+
+    private PShape createShapeTri(PShape r) {
+        PImage tex = m.loadImage("src\\main\\resources\\tex.jpg");
+        PShape s = m.createShape();
+        s.beginShape(m.TRIANGLES);
+        s.noStroke();
+        s.texture(tex);
+        s.textureMode(m.NORMAL);
+        for (int i=100; i<r.getChildCount (); i++) {
+            if (r.getChild(i).getVertexCount() ==3) {
+                for (int j=0; j<r.getChild (i).getVertexCount(); j++) {
+                    PVector p = r.getChild(i).getVertex(j);
+                    PVector n = r.getChild(i).getNormal(j);
+                    float u = r.getChild(i).getTextureU(j);
+                    float v = r.getChild(i).getTextureV(j);
+                    s.normal(n.x, n.y, n.z);
+                    s.vertex(p.x, p.y, p.z, u, v);
+                }
+            }
+        }
+        s.endShape();
+        return s;
     }
 
 }
